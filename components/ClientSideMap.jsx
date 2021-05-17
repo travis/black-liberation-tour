b945d3e8347c9106ddf2a9ef354320a486857031
Travis Vachon <travis.vachon@gmail.com>
2021-05-17 16:55:16 -0700

    add links to tour map

--- components/ClientSideMap.jsx
+++ components/ClientSideMap.jsx
@@ -1,10 +1,11 @@
 
 
+import Link from 'next/link'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const center = [37.820064, -122.273499]
 
-export default function Map({markers, ...props}) {
+export default function Map({ markers, ...props }) {
 
   return (
     <MapContainer scrollWheelZoom={false} {...props}>
@@ -12,12 +13,16 @@ export default function Map({markers, ...props}) {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
-      {markers && markers.map(([lat, long, title, addressTitle]) => (
+      {markers && markers.map(([lat, long, title, addressTitle, slug]) => (
         <Marker position={[lat, long]}>
-        <Popup>
-          {title} - {addressTitle}
-        </Popup>
-      </Marker>
+          <Popup>
+            <Link href={`/places/${slug}`}>
+              <a>
+                {title} - {addressTitle}
+              </a>
+            </Link>
+          </Popup>
+        </Marker>
       ))}
 
     </MapContainer>
