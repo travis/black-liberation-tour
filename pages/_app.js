0440f3ead0e41b7b642db639cdb5a7da219ff198
Travis Vachon <travis.vachon@gmail.com>
2021-05-31 19:13:12 -0700

    fix typo

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-PVMS731GQN" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `>
+            __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
