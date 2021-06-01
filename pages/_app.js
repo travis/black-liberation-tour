71d407a72b2adcc9b9df3fd443e7b4dbd1c2db01
Travis Vachon <travis.vachon@gmail.com>
2021-05-31 18:20:22 -0700

    fix warning

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,10 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossorigin="" />
+          crossOrigin="" />
         <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
           integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-          crossorigin=""></script>
+          crossOrigin=""></script>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-PVMS731GQN" />
         <script
           dangerouslySetInnerHTML={{
