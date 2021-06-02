54185de88bfca450140069e51c0cf4f7a6f5cd5b
Travis Vachon <travis.vachon@gmail.com>
2021-06-02 11:02:38 -0700

    rough pass on syncing up to new tour page designs

--- pages/tour.jsx
+++ pages/tour.jsx
@@ -5,103 +5,131 @@ import Map from '../components/Map'
 import { center, tourLocations } from "../lib/data"
 
 
-export default function Home() {
+export default function Tour() {
   return (
     <div className="">
-      <main className="p-12">
-        <h1 className="text-6xl text-center p-6">Black Liberation Walking Tour</h1>
-        <Map className="h-96" zoom={15} center={center} markers={tourLocations}/>
-        <div className="mt-12 flex flex-row">
-        <Link href="/places/st-augustines">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
-                  St. Augustine's Episcopal Church
+      <main className="flex flex-col lg:flex-row">
+        <div className="flex flex-col lg:w-1/3 text-white bg-black">
+          <h1 className="text-4xl text-center p-6">Black Liberation Walking Tour</h1>
+          <Map className="h-96 flex-grow" zoom={15} center={center} markers={tourLocations} />
+        </div>
+        <div className="flex flex-col sm:flex-row lg:w-2/3">
+          <div className="flex flex-col sm:w-1/2 text-left px-6 pt-24 bg-yellow-600">
+            <h3 className="font-serif text-3xl pl-3 mb-6">Stations:</h3>
+            <Link href="/places/st-augustines">
+              <a className="station-name">
+                <h2 className="">
+                  St. Augustine's<br />
+                 Episcopal Church
                 </h2>
-                <Image src="/places/st-augustines.jpg" height={222} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/annette-miller">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </a>
+            </Link>
+            <Link href="/places/annette-miller">
+              <a className="station-name">
+                <h2 className="">
                   Annette Miller
                 </h2>
-                <Image src="/places/annette-miller.jpg" height={200} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/freeways">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </a>
+            </Link>
+            <Link href="/places/freeways">
+              <a className="station-name">
+                <h2 className="">
                   Surrounded By Freeways
-                </h2>
-                <Image src="/places/freeways.jpg" height={222} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/delilah-beasley-house">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </h2>
+              </a>
+            </Link>
+            <Link href="/places/delilah-beasley-house">
+              <a className="station-name">
+                <h2 className="">
                   Delilah Beasley House
-                </h2>
-                <Image src="/places/delilah-beasley-house.jpg" height={380} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/cl-dellums">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </h2>
+              </a>
+            </Link>
+            <Link href="/places/cl-dellums">
+              <a className="station-name">
+                <h2 className="">
                   C.L. Dellums
                 </h2>
-                <Image src="/places/cl-dellums.jpg" height={222} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/ymca">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </a>
+            </Link>
+            <Link href="/places/ymca">
+              <a className="station-name">
+                <h2 className="">
                   Northwest Branch YMCA
                 </h2>
-                <Image src="/places/ymca.jpg" height={241} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/north-oakland-library">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
-                  North Oakland Branch Library
+              </a>
+            </Link>
+            <Link href="/places/north-oakland-library">
+              <a className="station-name">
+                <h2 className="">
+                  North Oakland<br />
+                Branch Library
                 </h2>
-                <Image src="/places/north-oakland-library.jpg" height={222} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/flints">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </a>
+            </Link>
+            <Link href="/places/flints">
+              <a className="station-name">
+                <h2 className="">
                   Flint’s BBQ
                 </h2>
-                <Image src="/places/flints.jpg" height={222} width={300} />
-              </div>
-            </a>
-          </Link>
-          <Link href="/places/california-hotel">
-            <a className="p-6">
-              <div>
-                <h2 className="text-3xl text-center mb-6">
+              </a>
+            </Link>
+            <Link href="/places/california-hotel">
+              <a className="station-name">
+                <h2 className="">
                   California Hotel
                 </h2>
+              </a>
+            </Link>
+          </div>
+          <div className="px-6 sm:w-1/2 grid grid-rows-5 grid-cols-12 gap-2 bg-yellow-600">
+            <Link href="/places/st-augustines">
+              <a className="col-start-9 col-span-4">
+                <Image src="/places/st-augustines.jpg" height={222} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/annette-miller">
+              <a className="col-start-3 col-span-8">
+                <Image src="/places/annette-miller.jpg" height={200} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/flints">
+              <a className="col-start-0 col-span-6">
+                <Image src="/places/flints.jpg" height={222} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/freeways">
+              <a>
+                <Image src="/places/freeways.jpg" height={222} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/delilah-beasley-house">
+              <a className="col-start-9 col-span-4 transform translate-y-20">
+                <Image src="/places/delilah-beasley-house.jpg" height={380} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/cl-dellums">
+              <a className="">
+                <Image src="/places/cl-dellums.jpg" height={222} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/ymca">
+              <a className="col-start-3 col-span-6">
+                <Image src="/places/ymca.jpg" height={241} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/north-oakland-library">
+              <a>
+                <Image src="/places/north-oakland-library.jpg" height={222} width={300} />
+              </a>
+            </Link>
+            <Link href="/places/california-hotel">
+              <a>
                 <Image src="/places/california-hotel.jpg" height={222} width={300} />
-              </div>
-            </a>
-          </Link>
+              </a>
+            </Link>
+          </div>
+
         </div>
       </main>
 
