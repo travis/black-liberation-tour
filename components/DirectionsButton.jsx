cba3f51ed4ac3b9bf3da611414208b31ad659cfe
Joe Pea <joe@trusktr.io>
2021-06-16 22:41:56 -0700

    button spacing and home page hotel image

--- components/DirectionsButton.jsx
+++ components/DirectionsButton.jsx
@@ -3,7 +3,10 @@ export default function DirectionsButton({ to, from }) {
     ? `engine=fossgis_osrm_foot&route=${from.lat}%2C${from.lng}%3B${to.lat}%2C${to.lng}`
     : `to=${to.lat}%2C${to.lng}`
   return (
-    <a href={`https://www.openstreetmap.org/directions?${query}`} className="btn">
+    <a
+      href={`https://www.openstreetmap.org/directions?${query}`}
+      className="btn p-10 lg:p-3 block text-center w-2/3 md:w-1/2"
+    >
       Get Directions
     </a>
   )
