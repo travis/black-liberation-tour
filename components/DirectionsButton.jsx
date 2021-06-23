c962ab380c3e4db7d89894cfce455f15b5fb8cf0
Gene Anderson <84358650+OurOakland@users.noreply.github.com>
2021-06-23 10:13:13 -0700

    Update DirectionsButton.jsx

--- components/DirectionsButton.jsx
+++ components/DirectionsButton.jsx
@@ -1,7 +1,7 @@
 export default function DirectionsButton({ to, from }) {
   const query = from
     ? `engine=fossgis_osrm_foot&route=${from.lat}%2C${from.lng}%3B${to.lat}%2C${to.lng}`
-    : `to=${to.lat}%2C${to.lng}`
+    : `engine=fossgis_osrm_foot&route=%3B${to.lat}%2C${to.lng}`
   return (
     <a
       href={`https://www.openstreetmap.org/directions?${query}`}
